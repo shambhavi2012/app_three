300b9527f238de4a448d8cf6904d13fccfb69889
shambhavi <shambhavi2012@yahoo.co.in>
2013-03-12 16:13:16 +0530

    App three

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppThree::Application.config.secret_token = '3f409e3b7099a89df8377bb648eeb329e5627bc8e9a7364187e237bd90e2396311baeed893c8d8cec6d6662db01c8a96b0958653f2fd77d9946cd908c9a503ab'
